47875ce69ce0f21aa3b1bb329d4f69f7d2de8aba
griffithcwood <griffwood@Griffiths-MacBook-Air-2.local>
2017-04-15 17:22:56 -0400

    People are strange

--- eclipse/RPG-Game/src/team_open_source/RPG_Game/logic/Door.java
+++ eclipse/RPG-Game/src/team_open_source/RPG_Game/logic/Door.java
@@ -1,5 +1,17 @@
 package team_open_source.RPG_Game.logic;
 
-public class Door {
+public class Door extends Thing {
 
+	public Door(int xPos, int yPos){
+	super(xPos, yPos);
 }
+
+
+public void next(){	
+}
+
+
+
+public String getImageName(){return "door.png";}
+}
+
