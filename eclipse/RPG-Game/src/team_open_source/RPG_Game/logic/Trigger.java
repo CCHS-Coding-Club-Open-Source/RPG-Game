69ec54fae65b0810747aac34789e394c79e799b2
griffithcwood <griffwood@Griffiths-MacBook-Air-2.local>
2017-04-15 14:54:49 -0400

    TIS BUT A REFACTOR

--- eclipse/RPG-Game/src/team_open_source/RPG_Game/logic/Trigger.java
+++ eclipse/RPG-Game/src/team_open_source/RPG_Game/logic/Trigger.java
@@ -1,5 +1,15 @@
 package team_open_source.RPG_Game.logic;
 
-public class Trigger {
+public interface Trigger  {
+static boolean triggered = false; 
+
+public void setPos();
+
+public void activate();
+
+public void set();
+
+
+	 
 
 }
