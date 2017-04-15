6b907cbfba7b41a20c796cfd2a4e992078d286d2
alset333 <alset333@icloud.com>
2017-04-15 12:21:14 -0400

    Graphic

--- eclipse/RPG-Game/src/team_open_source/RPG_Game/Main.java
+++ eclipse/RPG-Game/src/team_open_source/RPG_Game/Main.java
@@ -3,17 +3,24 @@
  */
 package team_open_source.RPG_Game;
 
+import javax.swing.JFrame;
+
 /**
+ * The main/runner class that is run by java to start the game
  * @author Team Open Source
+ * @version 0.1.0
  *
  */
 public class Main {
-
+	public static JFrame displayFrame;            //the frame will hold the JPanel 
+    public static GameDisplay displayPanel;     //the panel will hold the text or graphics
+    
 	/**
-	 * @param args
+	 * @param args The arguments given when running the program.
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
 
 	}
 
