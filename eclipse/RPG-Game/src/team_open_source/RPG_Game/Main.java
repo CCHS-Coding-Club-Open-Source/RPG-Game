5a477f7ed44acd15caf7633bfdfe5bc9b39d3dae
alset333 <alset333@icloud.com>
2017-04-15 11:34:15 -0400

    Created "Main.java"

---
+++ eclipse/RPG-Game/src/team_open_source/RPG_Game/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package team_open_source.RPG_Game;
+
+/**
+ * @author Team Open Source
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
