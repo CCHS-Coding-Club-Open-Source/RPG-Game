16420a012d5d4c6862d05c620198820ebe6b12b4
alset333 <alset333@icloud.com>
2017-04-15 16:42:50 -0400

    Get set pos for thing

--- eclipse/RPG-Game/src/team_open_source/RPG_Game/logic/Thing.java
+++ eclipse/RPG-Game/src/team_open_source/RPG_Game/logic/Thing.java
@@ -14,6 +14,23 @@ int yPos;
 	}
 	
 	public abstract String getImageName();
+
+	public int getxPos() {
+		return xPos;
+	}
+
+	public void setxPos(int xPos) {
+		this.xPos = xPos;
+	}
+
+	public int getyPos() {
+		return yPos;
+	}
+
+	public void setyPos(int yPos) {
+		this.yPos = yPos;
+	}
+	
 		
 	
 }
